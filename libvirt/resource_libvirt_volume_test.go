08aa2c506e17b5e05b934556a92db3c344d281a4
Duncan Mac-Vicar P <dmacvicar@suse.de>
2016-03-07 08:55:03 +0100

    add test for volume resource

---
+++ libvirt/resource_libvirt_volume_test.go
@@ -0,0 +1,92 @@
+package libvirt
+
+import (
+	"fmt"
+	"github.com/hashicorp/terraform/helper/resource"
+	"github.com/hashicorp/terraform/terraform"
+	"gopkg.in/alexzorin/libvirt-go.v2"
+	"testing"
+)
+
+func TestAccLibvirtVolume_Basic(t *testing.T) {
+	var volume libvirt.VirStorageVol
+
+	resource.Test(t, resource.TestCase{
+		PreCheck:     func() { testAccPreCheck(t) },
+		Providers:    testAccProviders,
+		CheckDestroy: testAccCheckLibvirtVolumeDestroy,
+		Steps: []resource.TestStep{
+			resource.TestStep{
+				Config: testAccCheckLibvirtVolumeConfig_basic,
+				Check: resource.ComposeTestCheckFunc(
+					testAccCheckLibvirtVolumeExists("libvirt_volume.terraform-acceptance-test-1", &volume),
+					resource.TestCheckResourceAttr(
+						"libvirt_volume.terraform-acceptance-test-1", "name", "terraform-test"),
+					resource.TestCheckResourceAttr(
+						"libvirt_volume.terraform-acceptance-test-1", "size", "1"),
+				),
+			},
+		},
+	})
+}
+
+func testAccCheckLibvirtVolumeDestroy(s *terraform.State) error {
+	virConn := testAccProvider.Meta().(*Client).libvirt
+
+	for _, rs := range s.RootModule().Resources {
+		if rs.Type != "libvirt_volume" {
+			continue
+		}
+
+		_, err := virConn.LookupStorageVolByKey(rs.Primary.ID)
+		if err == nil {
+			return fmt.Errorf(
+				"Error waiting for volume (%s) to be destroyed: %s",
+				rs.Primary.ID, err)
+		}
+	}
+
+	return nil
+}
+
+func testAccCheckLibvirtVolumeExists(n string, volume *libvirt.VirStorageVol) resource.TestCheckFunc {
+	return func(s *terraform.State) error {
+		virConn := testAccProvider.Meta().(*Client).libvirt
+
+		rs, ok := s.RootModule().Resources[n]
+		if !ok {
+			return fmt.Errorf("Not found: %s", n)
+		}
+
+		if rs.Primary.ID == "" {
+			return fmt.Errorf("No libvirt volume key ID is set")
+		}
+
+		retrievedVol, err := virConn.LookupStorageVolByKey(rs.Primary.ID)
+		if err != nil {
+			return err
+		}
+		fmt.Printf("The ID is %s", rs.Primary.ID)
+
+		realId, err := retrievedVol.GetKey()
+		if err != nil {
+			return err
+		}
+
+		if realId != rs.Primary.ID {
+			return fmt.Errorf("Resource ID and volume key does not match")
+		}
+
+		*volume = retrievedVol
+
+		return nil
+	}
+}
+
+const testAccCheckLibvirtVolumeConfig_basic = `
+resource "libvirt_volume" "terraform-acceptance-test-1" {
+    name = "terraform-test"
+    size = 1
+}
+`
+
