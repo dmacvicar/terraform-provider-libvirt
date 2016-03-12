e90ebeb14f79f0f79e351f80ccb6f4b27186f4a3
Duncan Mac-Vicar P <dmacvicar@suse.de>
2016-03-13 00:05:19 +0100

    fix count example

--- examples/count/libvirt.tf
+++ examples/count/libvirt.tf
@@ -8,13 +8,13 @@ resource "libvirt_volume" "opensuse_leap" {
 }
 
 resource "libvirt_volume" "volume" {
-  name = "volume"
+  name = "volume-${count.index}"
   base_volume = "${libvirt_volume.opensuse_leap.id}"
-#  count = 4
+  count = 4
 }
 
-resource "libvirt_domain" "terraform_test" {
-  name = "terraform_test"
+resource "libvirt_domain" "domain" {
+  name = "domain-${count.index}"
   disk {
        volume_id = "${element(libvirt_volume.volume.*.id, count.index)}"
   }
