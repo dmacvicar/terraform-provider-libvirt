18512360ba690362e3593193df3442d2eddc7964
Flavio Castelli <fcastelli@suse.com>
2016-06-27 11:35:39 +0200

    Refactoring: move code removal into a dedicated function
    This function is going to be shared later on with the cloud-init
    resource.
    
    Signed-off-by: Flavio Castelli <fcastelli@suse.com>

--- libvirt/utils.go
+++ libvirt/utils.go
@@ -5,6 +5,7 @@ import (
 	"crypto/rand"
 	"encoding/xml"
 	"fmt"
+	libvirt "github.com/dmacvicar/libvirt-go"
 	"log"
 	"net"
 	"strings"
@@ -86,4 +87,41 @@ func xmlMarshallIndented(b interface{}) (string, error) {
 		fmt.Errorf("could not marshall this:\n%s", spew.Sdump(b))
 	}
 	return buf.String(), nil
+}
+
+// Remove the volume identified by `key` from libvirt
+func RemoveVolume(virConn *libvirt.VirConnection, key string) error {
+	volume, err := virConn.LookupStorageVolByKey(key)
+	if err != nil {
+		return fmt.Errorf("Can't retrieve volume %s", key)
+	}
+	defer volume.Free()
+
+	// Refresh the pool of the volume so that libvirt knows it is
+	// not longer in use.
+	volPool, err := volume.LookupPoolByVolume()
+	if err != nil {
+		return fmt.Errorf("Error retrieving pool for volume: %s", err)
+	}
+	defer volPool.Free()
+
+	WaitForSuccess("Error refreshing pool for volume", func() error {
+		return volPool.Refresh(0)
+	})
+
+	// Workaround for redhat#1293804
+	// https://bugzilla.redhat.com/show_bug.cgi?id=1293804#c12
+	// Does not solve the problem but it makes it happen less often.
+	_, err = volume.GetXMLDesc(0)
+	if err != nil {
+		return fmt.Errorf("Can't retrieve volume %s XML desc: %s", key, err)
+	}
+
+	err = volume.Delete(0)
+	if err != nil {
+		return fmt.Errorf("Can't delete volume %s: %s", key, err)
+	}
+
+	return nil
+
 }
