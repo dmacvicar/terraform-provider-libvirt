f6e8160f13ffa0414d97a2d4e77bf3a399d5f32f
Flavio Castelli <fcastelli@suse.com>
2017-03-03 21:22:44 +0100

    Test coverage for pool sync

---
+++ libvirt/pool_sync_test.go
@@ -0,0 +1,46 @@
+package libvirt
+
+import (
+	"testing"
+)
+
+func TestAcquireLock(t *testing.T) {
+	ps := NewLibVirtPoolSync()
+
+	ps.AcquireLock("test")
+
+	_, found := ps.PoolLocks["test"]
+
+	if !found {
+		t.Errorf("lock not found")
+	}
+}
+
+func TestReleaseLock(t *testing.T) {
+	ps := NewLibVirtPoolSync()
+
+	ps.AcquireLock("test")
+
+	_, found := ps.PoolLocks["test"]
+	if !found {
+		t.Errorf("lock not found")
+	}
+
+	ps.ReleaseLock("test")
+	_, found = ps.PoolLocks["test"]
+	if !found {
+		t.Errorf("lock not found")
+	}
+}
+
+func TestReleaseNotExistingLock(t *testing.T) {
+	ps := NewLibVirtPoolSync()
+
+	ps.ReleaseLock("test")
+	_, found := ps.PoolLocks["test"]
+	if found {
+		t.Errorf("lock found")
+	}
+	// moreover there should be no runtime error because
+	// we are not trying to unlock a not-locked mutex
+}
