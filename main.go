6df6c15e5e465dc342169bad858262fb2eeb0f30
Zbigniew Kostrzewa <kostrzewa@9livesdata.com>
2017-06-15 13:17:02 +0200

    Seed RNG with current time.

--- main.go
+++ main.go
@@ -3,10 +3,16 @@ package main
 import (
 	"github.com/dmacvicar/terraform-provider-libvirt/libvirt"
 	"github.com/hashicorp/terraform/plugin"
+	"math/rand"
+	"time"
 )
 
 func main() {
 	plugin.Serve(&plugin.ServeOpts{
 		ProviderFunc: libvirt.Provider,
 	})
+}
+
+func init() {
+	rand.Seed(time.Now().UTC().UnixNano())
 }
