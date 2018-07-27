0422adc837c4acaeb50c4dbfb6e80e16177a9e88
Duncan Mac-Vicar P <dmacvicar@suse.de>
2018-07-27 23:12:50 +0200

    Revert "Remove ExpectNonEmptyPlan from test"
    This reverts commit b781a36992c52c91f8da804828078d5b94547d9f.

--- libvirt/resource_libvirt_domain_test.go
+++ libvirt/resource_libvirt_domain_test.go
@@ -385,6 +385,7 @@ func TestAccLibvirtDomain_CheckDHCPEntries(t *testing.T) {
 			},
 			resource.TestStep{
 				Config: configWithDomain,
+				ExpectNonEmptyPlan: true,
 				Check: resource.ComposeTestCheckFunc(
 					testAccCheckLibvirtDomainExists("libvirt_domain.acceptance-test-domain", &domain),
 				),
