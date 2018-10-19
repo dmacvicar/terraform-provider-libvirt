c92edd08c4ee48c1aa7966943f76886ff57317e5
Duncan Mac-Vicar P <dmacvicar@suse.de>
2018-10-19 16:46:09 +0200

    Add test for XSLT tranformation

---
+++ libvirt/utils_xslt_test.go
@@ -0,0 +1,37 @@
+package libvirt
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+const (
+	identitySpaceStripXSLT = `
+<xsl:stylesheet version="1.0" xmlns:xsl="http://www.w3.org/1999/XSL/Transform">
+  <xsl:strip-space elements="*" />
+  <xsl:template match="@*|node()">
+    <xsl:copy>
+      <xsl:apply-templates select="@*|node()"/>
+    </xsl:copy>
+  </xsl:template>
+</xsl:stylesheet>
+`
+)
+
+func TestTransformXML(t *testing.T) {
+	const inXml string = `    <foo>
+
+      <la two="two" one="one">foo this is a test</la>
+
+  <be>bebe  </be>
+  </foo>
+`
+	const outXml string = `<?xml version="1.0"?>
+<foo><la two="two" one="one">foo this is a test</la><be>bebe  </be></foo>
+`
+
+	result, err := transformXML(inXml, identitySpaceStripXSLT)
+	assert.Nil(t, err)
+	assert.Equal(t, outXml, result)
+}
