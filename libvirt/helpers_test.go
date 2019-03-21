9776560c91df2c2fe7c323ab28fa8bba7f5206d9
dmaiocchi <dmaiocchi@suse.com>
2019-03-21 13:50:01 +0100

    minor: don't print out unneeded info.
    In the helper function, we don't need to print out connection on helper test.
    
    This is more coherent with other codebase calls.

--- libvirt/helpers_test.go
+++ libvirt/helpers_test.go
@@ -118,7 +118,6 @@ func testAccCheckNetworkExists(name string, network *libvirt.Network) resource.T
 		}
 
 		virConn := testAccProvider.Meta().(*Client).libvirt
-		fmt.Printf("%p", virConn)
 		networkRetrived, err := virConn.LookupNetworkByUUIDString(rs.Primary.ID)
 		if err != nil {
 			return err
