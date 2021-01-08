ca153e49c11cb1b2f6474e3630f94962e6a40330
Karl Skewes <karl.skewes@gmail.com>
2021-01-08 15:42:27 +1300

    port - pipe in goroutine so can close - TBC pipe required

--- libvirt/utils_volume.go
+++ libvirt/utils_volume.go
@@ -13,39 +13,39 @@ import (
 
 func newCopier(virConn *libvirt.Libvirt, volume *libvirt.StorageVol, size uint64) func(src io.Reader) error {
 	copier := func(src io.Reader) error {
-		// FIXME - validate behaviour
-		// https://github.com/digitalocean/go-libvirt/pull/63/files#
-
 		r, w := io.Pipe()
-		defer func() {
-			cerr := w.Close()
-			if cerr != nil {
-				log.Printf("Error while closing IO stream %s", cerr)
+		defer w.Close()
+
+		go func() error {
+			bytesCopied, err := io.Copy(w, src)
+
+			// if we get unexpected EOF this mean that connection was closed suddently from server side
+			// the problem is not on the plugin but on server hosting currupted images
+			if err == io.ErrUnexpectedEOF {
+				return fmt.Errorf("Error: transfer was unexpectedly closed from the server while downloading. Please try again later or check the server hosting sources")
 			}
+			if err != nil {
+				return fmt.Errorf("Error while copying source to volume %s", err)
+			}
+
+			log.Printf("%d bytes uploaded\n", bytesCopied)
+			if uint64(bytesCopied) != size {
+				return fmt.Errorf("Error during volume Upload. BytesCopied: %d != %d volume.size", bytesCopied, size)
+			}
+
+			return w.Close()
+
 		}()
+
+		/*
+			* FIXME: use alternate simpler implementation without pipe?
+				if err := virConn.StorageVolUpload(*volume, src, 0, size, 0); err != nil {
+					return fmt.Errorf("Error while uploading volume %s", err)
+				}
+		*/
 
 		if err := virConn.StorageVolUpload(*volume, r, 0, size, 0); err != nil {
 			return fmt.Errorf("Error while uploading volume %s", err)
-		}
-
-		bytesCopied, err := io.Copy(w, src)
-		cerr := w.Close()
-
-		// if we get unexpected EOF this mean that connection was closed suddently from server side
-		// the problem is not on the plugin but on server hosting currupted images
-		if err == io.ErrUnexpectedEOF {
-			return fmt.Errorf("Error: transfer was unexpectedly closed from the server while downloading. Please try again later or check the server hosting sources")
-		}
-		if err != nil {
-			return fmt.Errorf("Error while copying source to volume %s", err)
-		}
-		if cerr != nil {
-			return fmt.Errorf("Error while closing IO stream %s", cerr)
-		}
-
-		log.Printf("%d bytes uploaded\n", bytesCopied)
-		if uint64(bytesCopied) != size {
-			return fmt.Errorf("Error during volume Upload. BytesCopied: %d != %d volume.size", bytesCopied, size)
 		}
 
 		return nil
