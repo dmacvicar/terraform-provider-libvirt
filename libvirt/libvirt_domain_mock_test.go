8f2698204af5179065f8a967b35903d06be8a67d
Karl Skewes <karl.skewes@gmail.com>
2021-06-07 08:57:50 +1200

    port - update mock parameters to upstream type

--- libvirt/libvirt_domain_mock_test.go
+++ libvirt/libvirt_domain_mock_test.go
@@ -1,13 +1,9 @@
 package libvirt
 
-import (
-	libvirtc "github.com/libvirt/libvirt-go"
-)
-
 type DomainMock struct {
 	QemuAgentCommandResponse string
 }
 
-func (d DomainMock) QemuAgentCommand(command string, timeout libvirtc.DomainQemuAgentCommandTimeout, flags uint32) (string, error) {
+func (d DomainMock) QemuAgentCommand(command string, timeout int32, flags uint32) (string, error) {
 	return d.QemuAgentCommandResponse, nil
 }
