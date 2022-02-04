40edf5700eec18b50217916f69449696645546c8
Shafer Stockton <shaferstockton@gmail.com>
2022-02-04 13:26:18 -0800

    Bump version of dmacvicar/libvirt to latest

--- examples/v0.13/fcos/main.tf
+++ examples/v0.13/fcos/main.tf
@@ -3,7 +3,7 @@ terraform {
   required_providers {
     libvirt = {
       source  = "dmacvicar/libvirt"
-      version = "0.6.2"
+      version = "0.6.12"
     }
     ignition = {
       source = "terraform-providers/ignition"
