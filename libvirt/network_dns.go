dmacvicar/terraform-provider-libvirt

commit eef2d215d621d2c7c8907bf63847ea9280e19ea9

port to v2 sdk (#969)
Changes that were required to upgrade the SDK:

* DNS hosts is now a set and it is read from libvirt into the state
* DCHP enablement is now computed, and uses a workaround to be explicitly set or disabled by the user while being enabled by default, unfortunately, using `GetOkExists` for now
* cloudinit disks are read back, and a workaround (custom serial) is used to mark them as owned by the cloudinit setting. This workaround needs to be improved/cleaned up.
* Ignition provider uses the old v1 SDK and [my contribution to update it to v2](https://github.com/community-terraform-providers/terraform-provider-ignition/pull/15) has not been merged, therefore our tests refer to [my upgraded fork](https://github.com/dmacvicar/terraform-provider-ignition).
* Some settings are now read back from libvirt, like firmware, nvram, cpu mode, etc.

Future work could be to port the Create/Read/etc functions to their new equivalents, which have better support for Context and Diagnostics.

libvirt/network_dns.go
@@ -1,7 +1,6 @@
 package libvirt
 
 import (
-	"errors"
 	"fmt"
 	"net"
 	"reflect"
@@ -9,7 +8,7 @@ import (
 	"strconv"
 
 	libvirt "github.com/digitalocean/go-libvirt"
-	"github.com/hashicorp/terraform-plugin-sdk/helper/schema"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
 	"libvirt.org/go/libvirtxml"
 )
 
@@ -81,21 +80,19 @@ func updateDNSHosts(d *schema.ResourceData, meta interface{}, network libvirt.Ne
 				return fmt.Errorf("add %v: %s", newEntry, err)
 			}
 		}
-
-		d.SetPartial(hostsKey)
 	}
 
 	return nil
 }
 
 func parseNetworkDNSHostsChange(change interface{}) (entries []libvirtxml.NetworkDNSHost, err error) {
-	slice, ok := change.([]interface{})
+	slice, ok := change.(*schema.Set)
 	if !ok {
-		return entries, errors.New("not slice")
+		return entries, fmt.Errorf("not set %v", change)
 	}
 
 	mapEntries := map[string][]string{}
-	for i, entryInterface := range slice {
+	for i, entryInterface := range slice.List() {
 		entryMap, ok := entryInterface.(map[string]interface{})
 		if !ok {
 			return nil, fmt.Errorf("entry %d is not a map", i)
@@ -151,16 +148,14 @@ func parseNetworkDNSHostsChange(change interface{}) (entries []libvirtxml.Networ
 // from the network definition
 func getDNSHostsFromResource(d *schema.ResourceData) ([]libvirtxml.NetworkDNSHost, error) {
 	dnsHostsMap := map[string][]string{}
-	if dnsHostCount, ok := d.GetOk(dnsPrefix + ".hosts.#"); ok {
-		for i := 0; i < dnsHostCount.(int); i++ {
-			hostPrefix := fmt.Sprintf(dnsPrefix+".hosts.%d", i)
-
-			address := d.Get(hostPrefix + ".ip").(string)
+	if dnsHosts, ok := d.GetOk(dnsPrefix + ".hosts"); ok {
+		for _, hostEntry := range dnsHosts.(*schema.Set).List() {
+			hostEntryMap := hostEntry.(map[string]interface{})
+			address := hostEntryMap["ip"].(string)
 			if net.ParseIP(address) == nil {
 				return nil, fmt.Errorf("could not parse address '%s'", address)
 			}
-
-			dnsHostsMap[address] = append(dnsHostsMap[address], d.Get(hostPrefix+".hostname").(string))
+			dnsHostsMap[address] = append(dnsHostsMap[address], hostEntryMap["hostname"].(string))
 		}
 	}
 
@@ -208,12 +203,13 @@ func getDNSForwardersFromResource(d *schema.ResourceData) ([]libvirtxml.NetworkD
 // getDNSEnableFromResource returns string to enable ("yes") or disable ("no") dns
 // in the network definition
 func getDNSEnableFromResource(d *schema.ResourceData) (string, error) {
-	if dnsLocalOnly, ok := d.GetOk(dnsPrefix + ".enabled"); ok {
-		if dnsLocalOnly.(bool) {
+	if dnsEnabled, ok := d.GetOk(dnsPrefix + ".enabled"); ok {
+		if dnsEnabled.(bool) {
 			return "yes", nil // this "boolean" must be "yes"|"no"
 		}
+		return "no", nil
 	}
-	return "no", nil
+	return "", nil
 }
 
 // getDNSSRVFromResource returns a list of libvirt's DNS SRVs

.github/workflows/test.yml
@@ -17,8 +17,14 @@ jobs:
   lint:
     name: Lint
     runs-on: ubuntu-latest
+    strategy:
+      matrix:
+        go-version: ["1.18.x"]
     steps:
-      - uses: actions/checkout@v2
+      - uses: actions/setup-go@v3
+        with:
+          go-version: ${{ matrix.go-version }}
+      - uses: actions/checkout@v3
         with:
           fetch-depth: 2
       - name: terraform fmt
@@ -28,6 +34,7 @@ jobs:
         uses: golangci/golangci-lint-action@v3
         with:
           version: v1.48.0
+          go-version: ${{ matrix.go-version }}
           only-new-issues: true
   build:
     name: Build
@@ -35,13 +42,13 @@ jobs:
     strategy:
       matrix:
         include:
-          - {os: ubuntu-latest, go: 1.16}
-          - {os: windows-latest, go: 1.16}
-          - {os: macos-latest, go: 1.16}
+          - {os: ubuntu-latest, go: 1.18}
+          - {os: windows-latest, go: 1.18}
+          - {os: macos-latest, go: 1.18}
     timeout-minutes: 10
     steps:
       - name: Set up Go
-        uses: actions/setup-go@v2
+        uses: actions/setup-go@v3
         with:
           go-version: ${{ matrix.go }}
         id: go

.golangci.yaml
@@ -24,5 +24,14 @@ linters:
     - errorlint
     - errcheck
     - gomnd
+
 issues:
   new-from-rev: HEAD~
+  exclude-rules:
+    - text: "Error return value of `d.Set` is not checked"
+      linters:
+        - errcheck
+    - text: "Magic number: 1024, in <argument> detected"
+      linters:
+        - gomnd
+

go.mod
@@ -2,22 +2,19 @@ module github.com/dmacvicar/terraform-provider-libvirt
 
 require (
 	github.com/c4milo/gotoolkit v0.0.0-20170704181456-e37eeabad07e // indirect
-	github.com/coreos/go-semver v0.3.0 // indirect
+	github.com/community-terraform-providers/terraform-provider-ignition/v2 v2.1.2
 	github.com/davecgh/go-spew v1.1.1
 	github.com/digitalocean/go-libvirt v0.0.0-20220616141158-7ed4ed4decd9
-	github.com/fatih/color v1.10.0 // indirect
 	github.com/google/uuid v1.1.2
-	github.com/hashicorp/go-getter v1.6.2 // indirect
-	github.com/hashicorp/terraform-plugin-sdk v1.9.0
+	github.com/hashicorp/terraform-plugin-sdk/v2 v2.21.0
 	github.com/hooklift/assert v0.0.0-20170704181755-9d1defd6d214 // indirect
 	github.com/hooklift/iso9660 v1.0.0
 	github.com/mattn/goveralls v0.0.2
 	github.com/pborman/uuid v1.2.0 // indirect
-	github.com/stretchr/testify v1.7.0
-	github.com/terraform-providers/terraform-provider-ignition v1.2.1
-	github.com/ulikunitz/xz v0.5.8 // indirect
-	golang.org/x/crypto v0.0.0-20220112180741-5e0467b6c7ce
+	github.com/stretchr/testify v1.7.2
+	golang.org/x/crypto v0.0.0-20220517005047-85d78b3ac167
 	golang.org/x/lint v0.0.0-20200302205851-738671d3881b
+	golang.org/x/sys v0.0.0-20220517195934-5e4e11fc645e // indirect
 	gopkg.in/yaml.v2 v2.4.0 // indirect
 	libvirt.org/go/libvirtxml v1.8003.0
 )
@@ -27,3 +24,5 @@ replace git.apache.org/thrift.git => github.com/apache/thrift v0.0.0-20180902110
 replace golang.org/x/crypto v0.0.0-20220112180741-5e0467b6c7ce => github.com/dmacvicar/golang-x-crypto v0.0.0-20220126233154-a96af8f07497
 
 go 1.13
+
+replace github.com/community-terraform-providers/terraform-provider-ignition/v2 => github.com/dmacvicar/terraform-provider-ignition/v2 v2.1.3-0.20210701165004-13acf61ca184

libvirt/config.go
@@ -7,7 +7,7 @@ import (
 
 	libvirt "github.com/digitalocean/go-libvirt"
 	uri "github.com/dmacvicar/terraform-provider-libvirt/libvirt/uri"
-	"github.com/hashicorp/terraform-plugin-sdk/helper/mutexkv"
+	"github.com/dmacvicar/terraform-provider-libvirt/libvirt/helper/mutexkv"
 )
 
 // Config struct for the libvirt-provider

libvirt/data_source_libvirt_network.go
@@ -5,8 +5,8 @@ import (
 	"net"
 	"strconv"
 
-	"github.com/hashicorp/terraform-plugin-sdk/helper/hashcode"
-	"github.com/hashicorp/terraform-plugin-sdk/helper/schema"
+	"github.com/dmacvicar/terraform-provider-libvirt/libvirt/helper/hashcode"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
 )
 
 // a libvirt network DNS host template datasource

libvirt/data_source_libvirt_network_test.go
@@ -4,8 +4,8 @@ import (
 	"fmt"
 	"testing"
 
-	"github.com/hashicorp/terraform-plugin-sdk/helper/resource"
-	"github.com/hashicorp/terraform-plugin-sdk/terraform"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/terraform"
 )
 
 func TestAccLibvirtNetworkDataSource_DNSHostTemplate(t *testing.T) {

commit 28f9924729a598a0a888079354bb9acb5548102f

Fix DNS enabled behavior
# tl;dr

This commit fixes a bug where a regression in the
`getDNSEnableFromResource` function made it impossible to turn off DNS
for libvirt networks

# Background

In libvirt, DNS is enabled by default in a network if unspecified.

In this provider, according to the
[docs](https://registry.terraform.io/providers/dmacvicar/libvirt/latest/docs/resources/network),
DNS is disabled if unspecified.

This means libvirt and the provider have opposing defaults for DNS
enabled. This is legitimate behavior and not the bug being fixed by this
PR.

# Issue 1

Commit eef2d215d caused a change in behavior where
libvirt-terraform-provider changed its behavior to be different from
what the docs says - it defaults to DNS enabled if unspecified.

# Cause

This is caused by returning an empty string in
`getDNSEnableFromResource` instead of `"no"`, which delegates the
defaulting to libvirt itself, but that's the wrong thing to do because
the default behavior of libvirt is to enable DNS when unspecified.

# Issue 2 (same cause)

The second return value of the Terraform SDK `GetOK` function is defined
as "whether or not the key has been set to a non-zero value at some
point". So even if the user explicitly sets DNS enabled to `false`, the
`GetOK` function would still return `false` as its second return value (since
`false` is considered a "zero" value for booleans) and so the `getDNSEnableFromResource`
function will return an empty string and DNS would get enabled

libvirt/network_dns.go
@@ -209,7 +209,7 @@ func getDNSEnableFromResource(d *schema.ResourceData) string {
 		}
 		return "no"
 	}
-	return ""
+	return "no"
 }
 
 // getDNSSRVFromResource returns a list of libvirt's DNS SRVs