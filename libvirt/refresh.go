2e6ca67824406f466b3ce9c523b6a40fb799d5c4
Duncan Mac-Vicar P. <duncan@mac-vicar.eu>
2024-09-19 23:05:15 +0200

    missing file

---
+++ libvirt/refresh.go
@@ -0,0 +1,16 @@
+package libvirt
+
+import (
+//libvirt "github.com/digitalocean/go-libvirt"
+//"github.com/hashicorp/terraform-plugin-sdk/v2/helper/retry"
+)
+
+const (
+	resourceStateConfSuccess   = "SUCCESS"
+	resourceStateConfError     = "ERROR"
+	resourceStateConfPending   = "PENDING"
+	resourceStateConfDone      = "DONE"
+	resourceStateConfActive    = "ACTIVE"
+	resourceStateConfNotExists = "NOT-EXISTS"
+	resourceStateConfExists    = "EXISTS"
+)
