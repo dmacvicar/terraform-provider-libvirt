ceee215ad3738cf9dd187d01669082191cc0b9bf
Memet Bilgin <memetb@gmail.com>
2024-10-23 05:49:54 -0300

    add test case for if os.UserHomeDir returns an error

--- libvirt/util/expandenv_test.go
+++ libvirt/util/expandenv_test.go
@@ -3,6 +3,7 @@ package util
 import (
 	"testing"
 	"strings"
+	"fmt"
 
 	"github.com/stretchr/testify/assert"
 )
@@ -20,4 +21,11 @@ func TestExpandEnvExt(t *testing.T) {
 	assert.Equal(t, "/home/mock/foo/bar/baz", ExpandEnvExt("~/foo/bar/baz"))
 	assert.Equal(t, "/home/mock/foo/bar/baz", ExpandEnvExt("${HOME}/foo/bar/baz"))
 	assert.Equal(t, "~foo/bar/baz", ExpandEnvExt("~foo/bar/baz"))
+
+	userHomeDir = func() (string, error) {
+		return "", fmt.Errorf("some failure")
+	}
+
+	// failure to get home expansion should leave string unchanged
+	assert.Equal(t, "~/foo/bar/baz", ExpandEnvExt("~/foo/bar/baz"))
 }
