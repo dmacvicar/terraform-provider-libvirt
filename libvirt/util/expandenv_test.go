206bf268ab60236e15250794e5fdb6dc78081b4a
Memet Bilgin <memetb@gmail.com>
2024-10-23 06:04:53 -0300

    update unit tests so that they are platform independent

--- libvirt/util/expandenv_test.go
+++ libvirt/util/expandenv_test.go
@@ -1,9 +1,10 @@
 package util
 
 import (
-	"testing"
-	"strings"
 	"fmt"
+	"path/filepath"
+	"strings"
+	"testing"
 
 	"github.com/stretchr/testify/assert"
 )
@@ -17,15 +18,15 @@ func TestExpandEnvExt(t *testing.T) {
 	}
 
 
-	assert.Equal(t, "foo/bar/baz", ExpandEnvExt("foo/bar/baz"))
-	assert.Equal(t, "/home/mock/foo/bar/baz", ExpandEnvExt("~/foo/bar/baz"))
-	assert.Equal(t, "/home/mock/foo/bar/baz", ExpandEnvExt("${HOME}/foo/bar/baz"))
-	assert.Equal(t, "~foo/bar/baz", ExpandEnvExt("~foo/bar/baz"))
+	assert.Equal(t, filepath.FromSlash("foo/bar/baz"), ExpandEnvExt("foo/bar/baz"))
+	assert.Equal(t, filepath.FromSlash("/home/mock/foo/bar/baz"), ExpandEnvExt("~/foo/bar/baz"))
+	assert.Equal(t, filepath.FromSlash("/home/mock/foo/bar/baz"), ExpandEnvExt("${HOME}/foo/bar/baz"))
+	assert.Equal(t, filepath.FromSlash("~foo/bar/baz"), ExpandEnvExt("~foo/bar/baz"))
 
 	userHomeDir = func() (string, error) {
 		return "", fmt.Errorf("some failure")
 	}
 
 	// failure to get home expansion should leave string unchanged
-	assert.Equal(t, "~/foo/bar/baz", ExpandEnvExt("~/foo/bar/baz"))
+	assert.Equal(t, filepath.FromSlash("~/foo/bar/baz"), ExpandEnvExt("~/foo/bar/baz"))
 }
