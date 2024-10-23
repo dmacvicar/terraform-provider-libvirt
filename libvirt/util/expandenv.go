f1950b49259ff07d011b95171fd7ff3aba7e91a2
Memet Bilgin <memetb@gmail.com>
2024-10-23 04:41:35 -0300

    update expandenv.go to use variables that can be mock patched

--- libvirt/util/expandenv.go
+++ libvirt/util/expandenv.go
@@ -1,11 +1,22 @@
 package util
 
+import (
+	"os"
+	"path/filepath"
+	"strings"
+)
+
+var (
+	userHomeDir = os.UserHomeDir
+	expandEnv   = os.ExpandEnv
+)
+
 // ExpandEnvExt expands environment variables and resolves ~ to the home directory
 // this is a drop-in replacement for os.ExpandEnv but is additionall '~' aware
 func ExpandEnvExt(path string) string {
-	path = os.ExpandEnv(path)
+	path = expandEnv(path)
 	if strings.HasPrefix(path, "~") {
-		home, err := os.UserHomeDir()
+		home, err := userHomeDir()
 		if err != nil {
 			return path // return path as-is if unable to resolve home directory
 		}
