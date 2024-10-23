bf8e95a101c185ae6bad632f555c845717eaf2e3
Memet Bilgin <memetb@gmail.com>
2024-10-23 10:18:45 -0300

    bugfix: make unittests pass on windows

--- libvirt/util/expandenv.go
+++ libvirt/util/expandenv.go
@@ -15,13 +15,20 @@ var (
 // this is a drop-in replacement for os.ExpandEnv but is additionally '~' aware.
 func ExpandEnvExt(path string) string {
 	path = filepath.Clean(expandEnv(path))
-	if strings.HasPrefix(path, "~/") {
+	tilde := filepath.FromSlash("~/")
+
+	// note to maintainers: tilde without a following slash character is simply
+	// interpreted as part of the filename (e.g. ~foo/bar != ~/foo/bar). However,
+	// when running on windows, the filepath will be represented by backslashes ('\'),
+	// therefore we need to convert "~/" to the platform specific format to test for
+	// it, otherwise on windows systems the prefix test will always fail.
+	if strings.HasPrefix(path, tilde) {
 		home, err := userHomeDir()
 		if err != nil {
 			return path // return path as-is if unable to resolve home directory
 		}
 		// Replace ~ with home directory
-		path = filepath.Join(home, strings.TrimPrefix(path, "~/"))
+		path = filepath.Join(home, strings.TrimPrefix(path, tilde))
 	}
 	return path
 }
